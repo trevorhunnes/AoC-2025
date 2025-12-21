198e9384333a90dcf81aec3fba224528b4c67baf
trevorhunnes <thunnes@hotmail.com>
2025-12-21 15:36:43 -0800

    Day 1, Problem 2

--- Day1/secretEntrance.py
+++ Day1/secretEntrance.py
@@ -3,28 +3,39 @@ def main():
     with open('./sequence.txt', 'r') as file:
         sequence = file.readlines()
 
+    test_sequence = ['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']
     print(get_code(sequence, START_LOCATION))
 
 def get_code(sequence, start):
     count = 0
     location = start
     for code in sequence:
+        # print(f"count : {count}")
         direction = code[0]
         distance = int(code[1:])
 
         if direction == 'L':
+            if location == 0:
+                count -= 1
             location -= distance
 
         elif direction == 'R':
             location += distance
 
-        if location >= 100:
-            while location >= 100:
+        if location > 100:
+            while location > 100:
+                # print(location)
+                count += 1
                 location = location - 100
 
         elif location < 0:
             while location < 0:
+                # print(location)
+                count += 1
                 location = 100 + location
+
+        if location == 100:
+            location = 0
 
         if location == 0:
             count += 1
