87c7b884f8f793e53f0ef8cea25615ed4a38caa8
trevorhunnes <thunnes@hotmail.com>
2025-12-28 17:20:27 -0700

    finished day 4 part 2

--- Day4/printing.py
+++ Day4/printing.py
@@ -2,20 +2,34 @@ def main():
     with open("./paper-grid.txt", "r") as file:
         grid = file.readlines()
 
-    print(get_count(grid))
+    count = 0
+    last_count = 0
+    removed = []
+    while True:
+        last_count, removed = get_count(grid)
+        if last_count == 0:
+            break
+        count += last_count
+        for c in removed:
+            my_string = grid[c[0]]
+            index = [c[1]]
+            grid[c[0]] = my_string[: index[0]] + "." + my_string[index[0] + 1 :]
+
+    print(count)
 
 
 def get_count(grid):
     count = 0
+    removed = []
     for x in range(0, len(grid)):
         # print(grid[x])
         for y in range(0, len(grid[x])):
             if grid[x][y] == "@":
                 if is_accessable(grid, x, y):
-                    # print(x, y)
+                    removed.append([x, y])
                     count += 1
 
-    return count
+    return count, removed
 
 
 def is_accessable(grid, x, y):
