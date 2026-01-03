f497361a842df303c579ebad82acf950751bee44
trevorhunnes <thunnes@hotmail.com>
2026-01-03 07:49:48 -0800

    finished day 5 part 2

--- Day5/cafeteria.py
+++ Day5/cafeteria.py
@@ -1,38 +1,66 @@
+clean_ranges: list[list[int]] = []
+
+
 def main():
     with open("./database.txt", "r") as file:
         database = file.read()
 
+    int_ranges: list[list[int]] = []
+    ranges: list[str] = []
+    ingredient_ids: list[str] = []
+
     ranges, ingredient_ids = split_database(database)
-    print(get_count(ranges, ingredient_ids))
+    for x in ranges:
+        split_ranges: list[int] = list(map(lambda y: int(y), x.split("-")))
+        int_ranges.append(split_ranges)
+    for c in sorted(int_ranges, key=lambda x: x[0]):
+        add_range(c)
+    print(get_count(clean_ranges))
 
 
-def split_database(database):
+def add_range(input_range: list[int]) -> None:
+    global clean_ranges
+    i: int = 0
+    if clean_ranges == []:
+        clean_ranges.append(input_range)
+    else:
+        for clean_range in clean_ranges:
+            i += 1
+            if input_range[0] >= clean_range[0] and input_range[0] <= clean_range[1]:
+                if input_range[1] <= clean_range[1]:
+                    break
+                else:
+                    clean_range[1] = input_range[1]
+                    break
+            elif input_range[1] >= clean_range[0] and input_range[1] <= clean_range[1]:
+                clean_range[1] = input_range[1]
+            elif input_range[0] < clean_range[0] and input_range[1] > clean_range[1]:
+                clean_range[0] = input_range[0]
+                clean_range[1] = input_range[1]
+            elif i == len(clean_ranges):
+                clean_ranges.append(input_range)
+
+
+def split_database(database: list[str]) -> tuple(list[str], list[str]):
     database = database.split("\n\n")
     ranges = database[0].split("\n")
     ingredient_ids = database[1].split("\n")
     return ranges, ingredient_ids
 
 
-def get_ranges(ranges):
-    range_set = set()
-    ranges = [c.split("-") for c in ranges]
-    for x in ranges:
-        for i in range(int(x[0]), int(x[1]) + 1):
-            range_set.add(i)
-
-    return range_set
+def optomize_ranges(ranges):
+    optomized_ranges = []
+    ranges = [c.spliy("-") for c in ranges]
+    ranges = [(int(item[0]), int(item[1])) for item in ranges]
 
 
-def get_count(ranges, ingredient_ids):
+def get_count(ranges):
     in_range = set()
-    ranges = [c.split("-") for c in ranges]
+    count = 0
     for x in ranges:
-        for id in ingredient_ids[:-1]:
-            if int(id) >= int(x[0]) and int(id) <= int(x[1]):
-                print("made it")
-                in_range.add(int(id))
+        count += (int(x[1]) - int(x[0])) + 1
 
-    return len(in_range)
+    return count
 
 
 main()
